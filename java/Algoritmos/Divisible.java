88a7f83d2ed99646c1d6b18631acc7d98e229499
Acertrox <acertrox@gmail.com>
2021-05-20 15:09:40 -0400

    adding java algorithms

---
+++ java/Algoritmos/Divisible.java
@@ -0,0 +1,14 @@
+package Algoritmos;
+public class Divisible {
+    public static void main(String[] args) {
+        int[] arreglo = new int[]{1,3,5,8,9,10,12,15,20};
+        for(int i=0; i<arreglo.length; i++){
+            if(arreglo[i]%5==0){
+                System.out.println(arreglo[i]+" si es divisible por 5");
+            }
+            else{
+                System.out.println(arreglo[i]+" no es divisible por 5");
+            }
+        }
+    }
+}
---
+++ java/Algoritmos/MuestraValores.java
@@ -0,0 +1,9 @@
+package Algoritmos;
+public class MuestraValores {
+    public static void main(String[] args) {
+        String[] nombres = new String[]{"Español", "Inglés", "Francés", "Alemán", "Japones", "Chino", "Hindi"};
+        for(int i=0; i<nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
