d3cb22e9725fdc1b835fa00eac4536a022bfc82e
Acertrox <acertrox@gmail.com>
2021-06-07 17:31:19 -0400

    updating prueba

--- springJava/prueba/src/main/java/com/diegosanchez/prueba/PruebaApplication.java
+++ springJava/prueba/src/main/java/com/diegosanchez/prueba/PruebaApplication.java
@@ -2,12 +2,23 @@ package com.diegosanchez.prueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PruebaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String Hello(){
+		return "Hello Client! How are you doing?";
+	}
+	@RequestMapping("/random")
+	public String HelloRandom(){
+		return "Spring Boot is great! So easy to just respond with Strings";
+	}
 }
