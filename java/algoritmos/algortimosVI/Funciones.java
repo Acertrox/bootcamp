b915733400eea2079796c07d6fd2104ddfbf48c8
Acertrox <acertrox@gmail.com>
2021-06-09 15:53:57 -0400

    updating bootcapm

---
+++ java/algoritmos/algortimosVI/Funciones.java
@@ -0,0 +1,29 @@
+
+
+public class Funciones {
+    public void sumaPorcentaje(double valor, int porcentaje){
+        double valorsuma = valor + (valor * porcentaje / 100);
+
+        System.out.println("Valor: "+valor);
+        System.out.println("Porcentaje: "+porcentaje+"% = "+valor*porcentaje/100);
+        System.out.println("Total: "+valorsuma);
+    }
+
+    public String invertirStr(String str){
+        String resultado = "";
+        for (int i = str.length()-1; i >=0; i--) {
+            resultado += str.charAt(i);
+        }
+        return resultado;
+    }
+
+    public int mayorArr(int[] arreglo){
+        int mayor = arreglo[0];
+        for (int i : arreglo) {
+            if (i>mayor) {
+                mayor = i;
+            }
+        }
+        return mayor;
+    }
+}
---
+++ java/algoritmos/algortimosVI/Index.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * Index
+ */
+public class Index {
+
+    public static void main(String[] args) {
+        Funciones funciones = new Funciones();
+        funciones.sumaPorcentaje(20, 15);
+
+        System.out.println(funciones.invertirStr("ojoD gnidoC"));
+
+        System.out.println(funciones.mayorArr(new int[]{1, 3, 5, 2}));
+    }
+
+
+
+}
